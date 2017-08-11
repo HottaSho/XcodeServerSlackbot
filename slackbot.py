c0a699f36e54623fe85c63c302f40f1cdaa00f6b
Mark Craft <markshocraft@gmail.com>
2017-08-11 16:10:42 -0400

    Update script to conform to PEP

--- slackbot.py
+++ slackbot.py
@@ -1,103 +1,122 @@
-import os, zipfile, plistlib, json, requests, time, re, shutil
+"""Python script that posts test results into slack."""
+
+import os
+import zipfile
+import plistlib
+import json
+import time
+import re
+import shutil
+import requests
+
 
 def change_directory():
-	# environment variables and paths
-	home = os.environ.get('HOME')
-	bot_name = os.environ.get('XCS_BOT_NAME').split()[0]
-	integration = os.environ.get('XCS_INTEGRATION_NUMBER')
-	output_directory = '%s/Downloads/%s' % (home, integration)
-	path = '/Library/Developer/XcodeServer/IntegrationAssets/'
+    """Change directory to where output plist is."""
+    # environment variables and paths
+    home = os.environ.get('HOME')
+    bot_name = os.environ.get('XCS_BOT_NAME').split()[0]
+    integration = os.environ.get('XCS_INTEGRATION_NUMBER')
+    output_directory = '%s/Downloads/%s' % (home, integration)
+    path = '/Library/Developer/XcodeServer/IntegrationAssets/'
 
-	os.chdir(path)
-	assets_files = os.listdir('.')
-	bot_folder = [i for i, s in enumerate(assets_files) if bot_name in s][0]
-	os.chdir('%s/%s' % (assets_files[bot_folder], integration))
+    os.chdir(path)
+    bot_folder = [s for s in os.listdir('.') if bot_name in s][0]
+    os.chdir('%s/%s' % (bot_folder, integration))
 
-	# Xcode finishes post script before bundle is created so have to wait
-	while not os.path.exists('xcodebuild_result.bundle.zip'):
-		time.sleep(1)
+    # Xcode finishes post script before bundle is created so have to wait
+    while not os.path.exists('xcodebuild_result.bundle.zip'):
+        time.sleep(1)
 
-	if not os.path.exists('%s/xcodebuild_result.bundle' % output_directory):
-		zip_ref = zipfile.ZipFile('xcodebuild_result.bundle.zip', 'r')
-		zip_ref.extractall('%s/' % output_directory)
-		zip_ref.close()
+    if not os.path.exists('%s/xcodebuild_result.bundle' % output_directory):
+        zip_ref = zipfile.ZipFile('xcodebuild_result.bundle.zip', 'r')
+        zip_ref.extractall('%s/' % output_directory)
+        zip_ref.close()
 
-	os.chdir('%s/xcodebuild_result.bundle' % output_directory)
+    os.chdir('%s/xcodebuild_result.bundle' % output_directory)
+
 
 def remove_directory():
-	integration = os.environ.get('XCS_INTEGRATION_NUMBER')
-	
-	os.chdir('../..')
-	shutil.rmtree(integration)
+    """Remove temporary directory."""
+    integration = os.environ.get('XCS_INTEGRATION_NUMBER')
+    os.chdir('../..')
+    shutil.rmtree(integration)
+
+
+def format_failure_text(text):
+    """Format failure text and return it."""
+    if 'Failure attempting to launch' in text:
+        return 'Failure attempting to launch'
+    return text.splitlines()[0]
+
 
 def parse_plist():
-	change_directory()
+    """Parse plist to get testcase information."""
+    change_directory()
 
-	data = {}
-	pl = plistlib.readPlist('Info.plist')
+    data = {}
+    plist = plistlib.readPlist('Info.plist')
 
-	# Add testcases
-	total_testcases = pl['TestsCount']
-	failed_testcases = pl['TestsFailedCount']
-	passed_testcases = total_testcases - failed_testcases
+    # Add testcases
+    failed_testcases = plist['TestsFailedCount']
+    passed_testcases = plist['TestsCount'] - failed_testcases
 
-	data_attachments = []
+    data_attachments = []
 
-	failure_summaries = pl.get('TestFailureSummaries', '')
-	for test in failure_summaries:
-		attachment = {}
-		testcase = test['TestCase']
-		testcase = testcase[2:len(testcase)-3].split()
-		message = test['Message']
+    failure_summaries = plist.get('TestFailureSummaries', '')
+    for test in failure_summaries:
+        attachment = {}
+        testcase = test['TestCase']
+        testcase = testcase[2:len(testcase) - 3].split()
+        message = test['Message']
 
-		attachment['color'] = 'danger'
-		attachment['title'] = re.sub(r"(\w)([A-Z])", r"\1 \2", testcase[1][4:])
-		attachment['text'] = message.splitlines()[0]
-		attachment['footer'] = testcase[0]
+        attachment['color'] = 'danger'
+        attachment['title'] = re.sub(r"(\w)([A-Z])", r"\1 \2", testcase[1][4:])
+        attachment['text'] = format_failure_text(message)
+        attachment['footer'] = testcase[0]
 
-		data_attachments.append(attachment)
+        data_attachments.append(attachment)
 
-	# Add initial text
-	devices = set()
-	devices_str = ''
-	for action in pl['Actions']:
-		device = action['RunDestination']['Name']
-		if not device in devices:
-			devices.add(device)
-			devices_str += ', %s' % device if len(devices_str) else device		
+    # Add setup and device information
+    devices = set()
+    devices_str = ''
+    for action in plist['Actions']:
+        device = action['RunDestination']['Name']
+        if device not in devices:
+            devices.add(device)
+            devices_str += ', %s' % device if devices_str else device
 
-	data_text = (
-			'Test Setup: %s | %s.\nTestcases: Passed: %s, Failed: %s' % 
-			(
-				str(os.environ.get('XCS_PRIMARY_REPO_BRANCH')),
-				devices_str,
-				str(passed_testcases),
-				str(failed_testcases)
-			)
-		)
+    data_text = (
+        'Test Setup: %s | %s.\nTestcases: Passed: %s, Failed: %s' %
+        (
+            str(os.environ.get('XCS_PRIMARY_REPO_BRANCH')),
+            devices_str,
+            str(passed_testcases),
+            str(failed_testcases)
+        )
+    )
 
-	# Create data dictionary
-	data['icon_emoji'] = ':thunder_cloud_and_rain:' if failed_testcases else ':unicorn_face:'
-	data['text'] = data_text
-	data['attachments'] = data_attachments
+    # Create dictionary for slackbot
+    data['icon_emoji'] = (
+        ':thunder_cloud_and_rain:' if failed_testcases else ':unicorn_face:'
+    )
+    data['text'] = data_text
+    data['attachments'] = data_attachments
 
-	remove_directory()
+    remove_directory()
 
-	return data
+    return data
 
 if __name__ == "__main__":
-	print('Starting bot...')
-	slack_url = os.environ.get('SLACK_WEBHOOK_URL')
-	slack_data = parse_plist()
-	print('Posting')
+    slack_url = os.environ.get('SLACK_WEBHOOK_URL')
+    slack_data = parse_plist()
 
-	# Post to slack
-	response = requests.post(
-	    slack_url, data=json.dumps(slack_data),
-	    headers={'Content-Type': 'application/json'}
-	)
-	if response.status_code != 200:
-	    raise ValueError(
-	        'Request to slack returned an error %s, the response is:\n%s'
-	        % (response.status_code, response.text)
-	    )
+    # Post to slack
+    response = requests.post(
+        slack_url, data=json.dumps(slack_data),
+        headers={'Content-Type': 'application/json'}
+    )
+    if response.status_code != 200:
+        raise ValueError(
+            'Request to slack returned an error %s, the response is:\n%s'
+            % (response.status_code, response.text)
+        )
